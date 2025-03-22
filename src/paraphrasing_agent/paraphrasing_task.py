6c8f201fa3a555754d862ae243fe2a0cd1e300bc
dhanush-bhargav <dhanushsbhargav@gmail.com>
2025-03-21 21:22:19 -0400

    remove 'yes_or_no' field from backend and database

--- src/paraphrasing_agent/paraphrasing_task.py
+++ src/paraphrasing_agent/paraphrasing_task.py
@@ -5,8 +5,7 @@ class ParaphrasingTask(Task):
     def __init__(self, topic_id, topic_name, agent):
         super().__init__(
             description=f"Paraphrase the arguments provided by the user for the topic: '{topic_name}' with topic_id: {topic_id} "
-                        f"after they have been validated by the Argument Validation Agent and add them to the database. "
-                        f"Do not alter the value of the 'yes_or_no' key, only update the value for the 'argument' key.",
+                        f"after they have been validated by the Argument Validation Agent and add them to the database.",
             expected_output="List of argument IDs.",
             agent=agent
         )
