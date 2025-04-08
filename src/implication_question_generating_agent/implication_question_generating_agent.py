cb242055ddf3d0aa6420d985897b80c95e4d2528
dhanush-bhargav <dhanushsbhargav@gmail.com>
2025-04-08 14:19:58 -0400

    changes to implication questions

--- src/implication_question_generating_agent/implication_question_generating_agent.py
+++ src/implication_question_generating_agent/implication_question_generating_agent.py
@@ -11,11 +11,10 @@ class ImplicationQuestionGeneratingAgent(Agent):
             goal="You will be given a central topic which is in the form of a policy change or action and a list of "
                  "arguments in favor of and against the central topic. "
                  "For each argument, you need to generate two questions:\n"
-                 "The first question should ask what the argument's impact on society would be and answerable as a "
-                 "choice of: positive, negative, or neutral. Call this 'IMPACT_QUESTION'.\n"
+                 "The first question should ask what the argument's overall impact on society would be. "
+                 "Call this 'IMPACT_QUESTION'.\n"
                  "The second question should ask how the central topic impacts the likelihood of the argument as an "
-                 "outcome and would be answerable as a choice of: more likely, less likely, or no impact. "
-                 "Call this 'LIKELIHOOD_QUESTION'.",
+                 "outcome. Call this 'LIKELIHOOD_QUESTION'.",
             llm=LLM(model=config_data.get_value('ImplicationQuestionGeneratingAgent', 'model_name'),
                     base_url=config_data.get_value('ImplicationQuestionGeneratingAgent', 'model_url')),
             backstory="You are an expert in framing questions given an argument and a central topic.",
